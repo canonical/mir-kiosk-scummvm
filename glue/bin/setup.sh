98fdd9add2768962192067b3d5d8afee584f3ea5
Alan Griffiths <alan@octopull.co.uk>
2019-09-11 11:17:42 +0100

    Fix setup

--- glue/bin/setup.sh
+++ glue/bin/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for PLUG in opengl pulseaudio alsa wayland; do sudo snap connect mir-kiosk-kodi:${PLUG}; done
+for PLUG in opengl pulseaudio alsa wayland; do sudo snap connect mir-kiosk-scummvm:${PLUG}; done
